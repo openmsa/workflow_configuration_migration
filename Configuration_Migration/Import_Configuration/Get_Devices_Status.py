5fa34a0d146fcc155ab248316a44c03c265c6fac
leyÃ <ley@ubiqube.com>
2022-02-07 17:01:42 +0000

    start get status

--- Configuration_Migration/Import_Configuration/Get_Devices_Status.py
+++ Configuration_Migration/Import_Configuration/Get_Devices_Status.py
@@ -35,7 +35,7 @@ context['status_liste_file_full'] = file
 
 wf_fields = {}
 warning = ""
-command = 'LIST'
+
 
 
 #get device_id from context
@@ -66,17 +66,17 @@ if data_list:
   for line in data_list:
     if (not line.startswith('#')) and line.strip():
       list = line.split('|')  
-      #  MS_to_run|parameter1_to_give_to_MS|parameter2_to_give_to_MS|ms_source|ms_source_field1|ms_source_field2
-      # interface_status|object_id||interface_status|object_id|
-      # bgp_neighbor_status|object_id|ip_bgp_neighbor|bgp_vrf|object_id|neighbor.0.bgp_vrf_neighbor            
-      if len(list) > 3:
-        ms_to_run                = list[0]
-        parameter1_to_give_to_ms = list[1]
-        parameter2_to_give_to_ms = list[2]
-        ms_source                = list[3]
-        ms_source_field1         = list[4]
-        ms_source_field2         = list[5]
-
+      #  ms_source|ms_source_field1|ms_source_field2|MS_to_run|parameter1_to_give_to_MS|parameter2_to_give_to_MS
+      #  interface|object_id|None|interface_status|object_id|None
+      #  bgp_vrf|object_id|neighbor.0.bgp_vrf_neighbor|bgp_neighbor_status|object_id|ip_bgp_neighbor          
+      if len(list) > 2:
+        ms_source                = list[0]
+        ms_source_field1         = list[1]
+        ms_source_field2         = list[2]
+        ms_to_run                = list[3]
+        parameter1_to_give_to_ms = list[4]
+        parameter2_to_give_to_ms = list[5]
+        
         if context.get(ms_source+'_values') and context[ms_source+'_values']:
           ''' "interface_values": {
                 "Port-channel1": {
@@ -98,16 +98,18 @@ if data_list:
             #fields = ms_source_field1.split('.0.')
             full_source_field = ms_source+'_'+ms_source_field1
             context['Status_'+full_source_field+'_field_values'] = {}
-            values_to_send = {}
+            context['Status_'+full_source_field+'_list'] = list
+            values_to_send = []
             ## Find all source values
             ms_values = context[ms_source+'_values']
             if isinstance(ms_values, dict):
               for  key, value1 in ms_values.items():
                 if isinstance(value1, dict):
                   if value1.get(ms_source_field1):
-                     field1_value = value1[ms_source_field1]
-                     values_to_send[field1_value] = {}
-                     if ms_source_field2:
+                     field1_value                = value1[ms_source_field1]
+                     new_value                   = {}
+                     new_value[parameter1_to_give_to_ms] = field1_value
+                     if ms_source_field2 and ms_source_field2 != 'None':
                        fields = ms_source_field2.split('.0.')
                        if isinstance(fields, typing.List) and fields and len(fields) > 1:
                          field_lev1 = fields[0]
@@ -117,34 +119,38 @@ if data_list:
                            if isinstance(value_lev1, dict):
                              for  key2, value2 in value_lev1.items():
                                if value2.get(field_lev2):
-                                 value_lev2 = value2[field_lev2] 
-                                 values_to_send[field1_value][value_lev2] = {}
+                                 new_value[parameter2_to_give_to_ms] = value2[field_lev2]                               
+                                 values_to_send.append(new_value)
+                                 new_value                   = {}
+                                 new_value[parameter1_to_give_to_ms] = field1_value
+
                      else:
-                       values_to_send[field1_value] = {}
+                       values_to_send.append(new_value)
+                       
+                       
             context['Status_'+full_source_field+'_field_values'] = values_to_send              
   
-            for field1_value, values in values_to_send.items():
+            #Run IMPORT for the give MS to update the DB
+            ms_input = {}
+            obj = {"":ms_input}  
+            params = {}
+            params[ms_to_run] = obj 
+            obmf.command_execute('IMPORT', params, timeout) #execute the MS to get new status
+
+  
+            for values in values_to_send:
               ms_input = {}
-              ms_input[parameter1_to_give_to_ms] = field1_value
-              if parameter2_to_give_to_ms and isinstance(values, dict):
-                for key2 in values.keys():
-                  ms_input[parameter2_to_give_to_ms] = key2
-                  obj = {"":ms_input}  
-                  params = dict(ms_to_run=obj)
-                  context['ms_params_'+parameter1_to_give_to_ms] = params
-
-                  obmf.command_execute(command, params, timeout) #execute the MS to get new status
-                  response = json.loads(obmf.content)
-                  context['ms_params_response_'+parameter1_to_give_to_ms] = response 
-              else:
+              for key,val in values.items():
+                ms_input[key] = val
+    
                 obj = {"":ms_input}  
-                params = dict(ms_to_run=obj)
-                context['ms_params_'+parameter1_to_give_to_ms] = params
-
-                obmf.command_execute(command, params, timeout) #execute the MS to get new status
+                params = {}
+                params[ms_to_run] = obj 
+                context['ms_params_'+ms_to_run+'_'+parameter1_to_give_to_ms] = params
+                obmf.command_execute('READ', params, timeout) #execute the MS to get new status
                 response = json.loads(obmf.content)
-                context['ms_params_response_'+parameter1_to_give_to_ms] = response 
-              
+                context['ms_params_response_'+ms_to_run+'_'+parameter1_to_give_to_ms] = response 
+
           else:
             warning = warning + "\n the first field should be object_id instead of '" +ms_source_field1+"'"
         else:
