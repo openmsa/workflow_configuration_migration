81568e4189380a7031c325907071f2f1ee23c2dc
ley <ley@ubiqube.com>
2022-02-08 20:29:43 +0000

    add reimport for real source device

--- Configuration_Migration/Import_Configuration/Get_ReImport_Parameters.py
+++ Configuration_Migration/Import_Configuration/Get_ReImport_Parameters.py
@@ -21,6 +21,24 @@ dev_var.add('interfaces.0.gil')
 dev_var.add('enable_filter')
 
 context = Variables.task_call(dev_var)
+
+#When the interfaces contains 'dot1q' and 'second_dot1q' we add it into the interface name
+if context.get('interfaces'):
+  new_interfaces = []
+  interfaces = context['interfaces']
+  for interface in interfaces:
+    if interface.get('destination') and interface.get('dot1q') and interface.get('second_dot1q'):
+      destination  = interface['destination']
+      dot1q        = interface['dot1q']
+      second_dot1q = interface['second_dot1q']
+      if dot1q and second_dot1q and not destination.endswith('.'+dot1q+second_dot1q):
+        interface['destination'] = destination+'.'+dot1q+second_dot1q 
+        context['change interface destination '+destination+ ' to'] = destination+'.'+dot1q+second_dot1q 
+  
+    new_interfaces.append(interface)
+    
+  context['interfaces'] = new_interfaces    
+    
     
 MSA_API.task_success('DONE', context, True)
 print(ret)
