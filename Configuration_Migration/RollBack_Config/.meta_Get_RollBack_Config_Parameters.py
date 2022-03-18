7cbbc503285c9e79c14390c781bb08ce05ab9573
root <antoine.brun@ubiqube.com>
2022-03-18 14:46:04 +0000

    WF review on rollback

--- Configuration_Migration/RollBack_Config/.meta_Get_RollBack_Config_Parameters.py
+++ Configuration_Migration/RollBack_Config/.meta_Get_RollBack_Config_Parameters.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1646215660483</value>
+            <value>1646755592023</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1646215660471</value>
+            <value>1646755592020</value>
         </entry>
         <entry>
             <key>TAG</key>
