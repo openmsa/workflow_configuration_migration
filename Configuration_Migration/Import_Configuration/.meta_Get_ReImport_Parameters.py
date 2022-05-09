630d4baa61bb38c02a54ff8b3d9365ce670e54c7
abr-ubiqube <antoine.brun@ubiqube.com>
2022-05-09 08:45:54 +0000

    fix test on batch_load var

--- Configuration_Migration/Import_Configuration/.meta_Get_ReImport_Parameters.py
+++ Configuration_Migration/Import_Configuration/.meta_Get_ReImport_Parameters.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1650400735113</value>
+            <value>1652084128416</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1650400735090</value>
+            <value>1652084128414</value>
         </entry>
         <entry>
             <key>TAG</key>
