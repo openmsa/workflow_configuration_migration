dcb15544df5432ad42472be92d72c716fa80bae4
ley <ley@ubiqube.com>
2022-09-20 11:44:24 +0000

    MSA-11846 : Get Devices Status MS doesnt need to run together with migration MS

--- Configuration_Migration/Import_Configuration/Import_Old_Device_Configuration.py
+++ Configuration_Migration/Import_Configuration/Import_Old_Device_Configuration.py
@@ -30,67 +30,116 @@ obmf  = Order(device_id=device_id)
 
 context['customer_id_instance_id'] =  context['customer_id'] + '_#' +context['SERVICEINSTANCEID']
 
-#we synchronise all MS attached to the source device because some MS are intermediated and need to be synchronized with good order.
-create_event(device_id_full, "5", "MIGRATION", "SYNCHRONIZE", subtenant_ref, subtenant_id, "IMPORT:START:"+device_id_full)
-
-obmf.command_synchronize(timeout)
-responses = json.loads(obmf.content)
-#context[ 'ALL source MS_synch_values for '+device_id] = responses
-if isinstance(responses, dict) and responses.get("wo_status") and responses["wo_status"] == "FAIL":
-  msg = 'Can not synchronise device '+ device_id_full + ' : '+responses['wo_newparams']
-  create_event(device_id_full, "1", "MIGRATION", "SYNCHRONIZE", subtenant_ref, subtenant_id, msg)
+#read import_WF_parameters_into_MS.txt file to get the list of MS to import:
+wf_path = os.path.dirname(__file__)
+file =  wf_path+'/../'+context['import_MS_list_file']   # Configuration/import_MS_list.txt
+if os.path.isfile(file):
+  file1 = open(file, "r")
+  # read file content
+  import_liste = file1.read()
+  file1.close()
+  data_list = import_liste.split('\n')
+  data_list = [i for i in data_list if i] #remove empty element
+else:
+  msg = 'Can not open file "' + file + '"'
+  create_event(device_id_full, "1", "MIGRATION", "IMPORT",  subtenant_ref, subtenant_id, msg)
   MSA_API.task_error(msg, context, True)
+  data_list = ''    
+  
+#context['import_MS_list_file_full'] = file 
+responses = []
+MS_wanted=[]
+if data_list:
+  MS_to_import={}
+  for line in data_list:
+    if (not line.startswith('#')) and line.strip():
+      list = line.split('|') 
+      #MS|Importrank(integer, 0=first)      
+      if len(list) > 1:
+        MS         = list[0]
+        importrank = list[1]
+        if not MS_to_import.get(importrank):
+          MS_to_import[importrank] = []
+        MS_to_import[importrank].append(MS)
+        MS_wanted.append(MS) 
+  
+# Get deployment settings ID for the device.
+deployment_settings_id = obmf.command_get_deployment_settings_id()
+context['source_deployment_settings_id_'+device_id_full] = deployment_settings_id
+
+#Get all microservices attached to this deployment setting.
+confprofile  = ConfProfile(deployment_settings_id)
+all_ms_attached = confprofile.read()
+all_ms_attached = json.loads(all_ms_attached)
+context['MS_attached source device_id' + device_id + ' : '] = all_ms_attached
+if all_ms_attached.get("microserviceUris"):
+  all_ms_attached_uris = all_ms_attached["microserviceUris"] 
+#all_ms_attached = {"id" : 44, ..."microserviceUris" : { "CommandDefinition/LINUX/CISCO_IOS_emu  },  "CommandDefinition/LINUX/CISCO_IOS_emulation/bgp_vrf.xml" : {  "name" : "bgp_vrf",   "groups" : [ "EMULATION", "CISCO", "IOS" ].....
+else:
+  all_ms_attached_uris={}   
+
+MS_list = []   
+if all_ms_attached_uris:
+  for full_ms, MS in all_ms_attached_uris.items():
+    if Path(full_ms).stem:
+      MS_list.append(Path(full_ms).stem)    # Path(full_ms).stem = MS filename without extension
+   
+if MS_list:
+  MS_list_string             = ';'.join(MS_list)  
+  context['MS_list']  = MS_list_string
+else:
+  MS_list = ''
+    
+#Check if missing MS in the deploiement setting :
+missing_MS_in_deployment_setting = []
+for MS in MS_wanted:
+  if not MS in MS_list:
+    missing_MS_in_deployment_setting.append(MS)
+  
+if missing_MS_in_deployment_setting:  
+  msg = 'Some MS ('+', '.join(missing_MS_in_deployment_setting) + ') are missing in the deployment settings "'+all_ms_attached['name']+' (id='+str(all_ms_attached['id'])+')" for device '+ device_id_full +', please add theim'
+  create_event(device_id_full, "5", "MIGRATION", "SYNCHRONIZE", subtenant_ref, subtenant_id, msg)
+  MSA_API.task_error(msg, context, True)
+  
+# The import is order by importrank of the MS. 
+# So we import all MS at the same times which have the same importrank. 
+    
+for importrank in sorted(MS_to_import):
+  params = {}
+  for MS in MS_to_import[importrank]:
+    obj = {}
+    #obj['object_id']    = MS  
+    params[MS] = obj 
+  obmf.command_execute('IMPORT', params, timeout) #execute the MS to get new status
+  response = json.loads(obmf.content)
+
+  if (response.get("status") and response["status"] == "OK") or (response.get("wo_status") and response["wo_status"] == "OK"):
+    if response.get("status"):
+      response =  response["message"]
+    else:
+      response =  response["wo_newparams"]
+    responses.append(response)
+  else:
+    msg = 'Can not synchronise device '+ device_id_full + ' : '+ str(response)
+    create_event(device_id_full, "1", "MIGRATION", "SYNCHRONIZE", subtenant_ref, subtenant_id, msg)
+    MSA_API.task_error(msg, context, True)
+
 
 create_event(device_id_full, "5", "MIGRATION", "SYNCHRONIZE", subtenant_ref, subtenant_id, "IMPORT:END:"+device_id_full)
 
 
-MS_list = []
 if isinstance(responses, typing.List): 
   #responses contains only MS which contains some datas, we don't get attached MS without datas
   for response in responses:
-    # "commandId": 0, "status": "OK","message": "{\"class_map\":{\"RT\":{\"object_id\":\"RT\",\"matches\":{\"0\":{\"not\":\"\",\"match_cmd\":\"ip \"}}}},\"ip_route\"
-    if response.get('message') and response.get('status'):
-      if response['status'] != 'OK':
-        msg = 'ERROR: during synchronise. Managed entity Id: '+ device_id_full + ' : ' + str(response)
-        create_event(device_id_full, "1", "MIGRATION", "SYNCHRONIZE", subtenant_ref, subtenant_id, msg)
-        MSA_API.task_error(msg, context, True)
+    response_message = json.loads(response)  #convert into json array
+    for MS in response_message:
+      if response_message.get(MS):
+        context[ MS + '_values_serialized'] = json.dumps(response_message.get(MS))
       else:
-        response_message = json.loads(response.get('message'))  #convert into json array
-        for MS in response_message:
-          if response_message.get(MS):
-            #MS_list.append(MS)
-            context[ MS + '_values_serialized'] = json.dumps(response_message.get(MS))
-          else:
-            context[ MS + '_values_serialized'] = json.dumps(response_message)
+        context[ MS + '_values_serialized'] = json.dumps(response_message)
             
-            
-  # Get deployment settings ID for the device.
-  deployment_settings_id = obmf.command_get_deployment_settings_id()
-  context['source_deployment_settings_id_'+device_id_full] = deployment_settings_id
-  
-  #Get all microservices attached to this deployment setting.
-  confprofile  = ConfProfile(deployment_settings_id)
-  all_ms_attached = confprofile.read()
-  all_ms_attached = json.loads(all_ms_attached)
-  if all_ms_attached.get("microserviceUris"):
-    all_ms_attached = all_ms_attached["microserviceUris"] 
-  #context['MS_attached source device_id' + device_id + ' : '] = all_ms_attached
-  #all_ms_attached = {"id" : 44, ..."microserviceUris" : { "CommandDefinition/LINUX/CISCO_IOS_emu  },  "CommandDefinition/LINUX/CISCO_IOS_emulation/bgp_vrf.xml" : {  "name" : "bgp_vrf",   "groups" : [ "EMULATION", "CISCO", "IOS" ].....
-     
-  if all_ms_attached:
-    for full_ms, MS in all_ms_attached.items():
-      if Path(full_ms).stem:
-        MS_list.append(Path(full_ms).stem)    # Path(full_ms).stem = MS filename without extension
-   
-if MS_list:
-  MS_list             = ';'.join(MS_list)  
-  context['MS_list']  = MS_list
-else:
-  MS_list = ''
-    
-    
-    
-msg = 'DONE: all MS attached to the managed entity: '+ device_id_full + ' imported'
+
+msg = 'DONE: all MS attached to the managed entity: '+ device_id_full + ' imported ('+', '.join(MS_list) +')'
 create_event(device_id_full, "5", "MIGRATION", "SYNCHRONIZE", subtenant_ref, subtenant_id, msg)
 MSA_API.task_success(msg, context, True)
 
