b6877136217def14a089d07b3bcf14e3b3339240
JoseBalbuena <jbalbuen22@gmail.com>
2023-01-09 13:43:45 +0000

    HairPin

--- Configuration_Migration/Generate_Configuration/.meta_Push_Configuration.py
+++ Configuration_Migration/Generate_Configuration/.meta_Push_Configuration.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1670263708529</value>
+            <value>1673015225325</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1670263708516</value>
+            <value>1673015225310</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Configuration_Migration/Import_Configuration/.meta_Get_New_Migration_Parameters.py
+++ Configuration_Migration/Import_Configuration/.meta_Get_New_Migration_Parameters.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1653317754438</value>
+            <value>1672870822429</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1653317754436</value>
+            <value>1672870822391</value>
         </entry>
         <entry>
             <key>TAG</key>
